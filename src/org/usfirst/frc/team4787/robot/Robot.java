e73c58b5f605df5ed98de7e3375aa0cd1fa9105d
Jordan Lozinski <jalozinski@cps.edu>
2015-02-06 19:20:09 -0600

    Commit

--- src/org/usfirst/frc/team4787/robot/Robot.java
+++ src/org/usfirst/frc/team4787/robot/Robot.java
@@ -1,118 +1,145 @@
+/** 
+ *  FRC ROBOTICS TEAM 4787 - PROTOTYPE CODE
+ *  AUTHORS: SOFTWARE SUBTEAM MEMBERS
+ */
 
 package org.usfirst.frc.team4787.robot;
-
 
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
 public class Robot extends SampleRobot {
-    Joystick stick;
-    
-    final int FLEFT_PWM = 1, BLEFT_PWM = 2, FRIGHT_PWM = 3, BRIGHT_PWM = 4, PERP_PWM = 5, MECH1_PWM = 6, MECH2_PWM = 7; //perp = perpendicular to others
-    final int MECH_UP = 3, MECH_DOWN = 2; // buttons 
-    final int TRIGGER = 1;
+    Joystick drivestick, mechstick; JoystickButton trigger;
+    final int LEFT_PWM = 0, RIGHT_PWM = 1, PERP_PWM = 2, MECH1_PWM = 3, MECH2_PWM = 4, MECH_UP = 3, MECH_DOWN = 2, DRIVESTICK_NUM = 0, MECHSTICK_NUM = 1;
     final double DEADZONEX = 0.05, DEADZONEY = 0.05;
-    double expX, expY;
-    double m = 0.5;
-    double x, y, z;
-    boolean STRAFEMODE;
-    Jaguar fLeft = new Jaguar(FLEFT_PWM);
-    Jaguar bLeft = new Jaguar(BLEFT_PWM);
-    Jaguar fRight= new Jaguar(FRIGHT_PWM);
-    Jaguar bRight = new Jaguar(BRIGHT_PWM);
+    
+    double lastTime = 0, expX = 0, expY = 0, x = 0, y = 0, z = 0, mechY = 0;
+    boolean STRAFEMODE = true;
+    
+    Jaguar left = new Jaguar(LEFT_PWM);
+    Jaguar right = new Jaguar(RIGHT_PWM);
     Jaguar perp = new Jaguar(PERP_PWM);
     Jaguar mech1 = new Jaguar(MECH1_PWM);
     Jaguar mech2 = new Jaguar (MECH2_PWM);
-    
-    
-    public Robot() {
-    	stick = new Joystick(0);
+
+    /**
+    * Constructor for a 4787 Robot. 
+    * Initializes robot joysticks and camera. 
+    */
+    public Robot () {
+    	drivestick = new Joystick(DRIVESTICK_NUM);
+    	mechstick = new Joystick(MECHSTICK_NUM);
+    	trigger = new JoystickButton(drivestick, 1);
     	CameraServer camera = CameraServer.getInstance();
     	camera.setQuality(100);
     	camera.startAutomaticCapture("cam0");
     }
 
     /**
-     * Drive left & right motors for 2 seconds then stop
+     * Move forward.
+     * Called once each time the robot enters the autonomous state.
      */
-    public void autonomous() {
-    	while(true){
-    		System.out.println();
+    @Override
+	public void autonomous() {
+    	System.out.println("I believe I am currently in autonmous mode.");
+    	left.set(0.1); right.set(0.1);
+    }
+
+    /**
+     * A version of crab drive.
+     * Called once each time the robot enters the operator-controlled state.
+     */
+    @Override
+	public void operatorControl() {
+		/*
+ 		if (x > DEADZONEX || x < -DEADZONEX) {
+			expX = Math.pow(x, 3); // x^3 for nonlinear control
+			System.out.println("deadzonex");
+		}
+		if (y > DEADZONEY || y < -DEADZONEY) {
+			expY = Math.pow(y, 3); // y^3 for nonlinear control
+			System.out.println("deadzoney");
+		}
+		
+	    // Motor power settings
+		System.out.println((expX - expY)*z);
+		left.set((expX - expY) * z); 
+		System.out.println((expX + expY)*z);
+		right.set((expX + expY) * z);
+    	                             
+    	mechY = mechstick.getY();
+	    	                     
+	    	if (mechY >= DEADZONEY) {
+		    	mech1.set(mechY/2);
+		    	mech2.set(mechY/2);
+	    	}
+	    	
+		if (Math.abs(x) < DEADZONEX) {
+			perp.set(x * z);
+		}
+		if (Math.abs(y) < DEADZONEY) {
+			left.set(-y * z);
+			right.set(-y * z);
+
+		}
+		
+		*/
+    	
+    	while (true) {
+    		x = drivestick.getX();
+    		y = drivestick.getY();
+    		
+    		/*
+    	
+    		if(trigger.get())
+    		{
+    			if(lastTime - Timer.getFPGATimestamp() > 1)
+    			{
+    				lastTime = Timer.getFPGATimestamp();
+    				STRAFEMODE = !STRAFEMODE;
+    			}
+    		} 
+    		
+    		*/
+    		
+    		if(!STRAFEMODE)
+    		{
+    			System.out.println("Gnomes mode | x: " + x + "  y:" + y);
+    			left.set(x - y);
+    			right.set(x + y);
+    		}
+    		else
+    		{
+    			System.out.println("Strafe mode | x: " + x + "   y:" + y);
+    			left.set(-y);
+    			right.set(y);
+    			perp.set(-x);
+    		}
     	}
     }
 
     /**
-     * Runs the motors with arcade steering.
+     * Robot test mode.
      */
-    public void operatorControl() {
-    	while(true){
-    		x = stick.getX();
-    		y = stick.getY();
-    		z = -(stick.getZ()); // Throttle control
-    		STRAFEMODE = !stick.getRawButton(TRIGGER);
-	    	if (STRAFEMODE)
-	    	{
-	    		if(Math.abs(x) < DEADZONEX){
-	    			perp.set(x * z);
-	    		}
-	    		if(Math.abs(y) < DEADZONEY){
-	    			fLeft.set(-y * z);
-	    			bLeft.set(-y * z);
-	    			fRight.set(-y * z);
-	    			bRight.set(-y * z);
-	    		}
-	    	}
-	    	else
-	    	{
-	    		if(x>DEADZONEX || x<-DEADZONEX){
-	    			expX = Math.pow(x, 3); // x^3 for nonlinear control
-	    		}
-	    		if(y>DEADZONEY || y<-DEADZONEY){
-	    			expY = Math.pow(y, 3); // y^3 for nonlinear control
-	    		}
-	    	    // Motor power settings
-	    		fLeft.set((expX - expY) * z);
-	    		bLeft.set((expX - expY) * z);
-	    		fRight.set((expX + expY) * z);
-	    		bRight.set((expX + expY) * z);
-	    	}
-	    	if (stick.getRawButton(MECH_UP)){
-	    		mech1.set(m);
-	    		mech2.set(m);
-	    	}else if(stick.getRawButton(MECH_DOWN)){
-	    		mech1.set(-m);
-	    		mech2.set(-m);
-	    	}else{
-	    		mech1.set(0);
-	    		mech2.set(0);
-	    	}
+    @Override
+	public void test() {
+    	while (true) {
+			left.set(0.2); Timer.delay(2);
+			right.set(0.2); Timer.delay(2);
+			perp.set(0.2); Timer.delay(2);
+			mech1.set(0.2); Timer.delay(2);
+			mech2.set(0.2); Timer.delay(2);
     	}
-    }
-
-    public void test() {
-    	System.out.println("test method");
+  
     }
     
-    public void disabled()
+    @Override
+	public void disabled()
     {
-    	System.out.println("Ableism is alive");
+    	System.out.println("I'm disabled.");
     }
 }
